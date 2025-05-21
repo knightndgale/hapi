0c78b46eaabf1c16837e5bd3c63269c5c0cb692a
knightndgale <knightndgale@gmail.com>
2025-05-21 23:59:58 +0800

    sync

---
+++ directus-sync.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  directusUrl: "https://directus-production-8483.up.railway.app/",
+  directusEmail: "knightndgale@gmail.com",
+  directusPassword: "9ekt8xf2ulh7a7j1q9e4l7d125ket1wc",
+  dumpPath: "./directus/snapshots/directus-config",
+};
---
+++ directus/directus-sync.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  directusUrl: "http://localhost/directus",
+  directusEmail: "admin@admin.com",
+  directusPassword: "admin123",
+  dumpPath: "./snapshots/directus-config",
+};
